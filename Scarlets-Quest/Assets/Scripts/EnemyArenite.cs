6bc72d7d60dca9d7c2fd4531ad1148e31e1c0f1a
Migalhaz <miguel.chagas614@gmail.com>
2023-08-04 14:31:14 -0300

    falta barriga dagua

--- Scarlets-Quest/Assets/Scripts/EnemyArenite.cs
+++ Scarlets-Quest/Assets/Scripts/EnemyArenite.cs
@@ -6,6 +6,8 @@ public class EnemyArenite : MonoBehaviour
 {
     public float TimeToAttack;
     public GameObject fire2;
+    public Transform Firepoint;
+
     
     // Start is called before the first frame update
     void Start()
@@ -15,7 +17,7 @@ public class EnemyArenite : MonoBehaviour
 
     void Attack()
     {
-        Instantiate(fire2, transform.position + Vector3.left, Quaternion.identity);
+        Instantiate(fire2, Firepoint.position , Quaternion.identity);
 
     }
 }
